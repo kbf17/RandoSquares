47dcf3539a8ceb9f06fe6269a5c0d627dddd077e
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-06-20 17:51:02 -0500

    mouseovers change opacity

--- style.js
+++ style.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
 });
 
 var firstButton = document.createElement('button');
@@ -10,13 +11,33 @@ var firstButton = document.createElement('button');
     firstButton.style.float = 'left';
 document.body.appendChild(firstButton);
 
-var squares = ('div');
-var numberOfSquares = document.getElementsByTagName(squares);
+
 
 firstButton.addEventListener('click', function() {
     var squares = document.createElement('div');
     squares.className = ('squares');
     squares.id = (numberOfSquares.length);
     document.body.appendChild(squares);
+    squares.innerText = squares.id;
+    squares.addEventListener('mouseover', mouseOver)
+    squares.addEventListener('mouseout', mouseOut);
+    function mouseOver () {
+       squares.style.opacity = '1';
+    };
+    function mouseOut () {
+        squares.style.opacity = '0';
+    };
 })
 
+
+
+
+var squares = ('div');
+
+var numberOfSquares = document.getElementsByTagName(squares);
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+
+// squares.addEventListener('click', function() {
+//     squares.style.backgroundColor = randomColor;
+//     });
