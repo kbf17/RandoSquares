5daf9300e62c053d7a83afa1e765036597b3910a
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-06-21 17:56:47 -0500

    project complete

--- style.js
+++ style.js
@@ -9,18 +9,16 @@ var firstButton = document.createElement('button');
     firstButton.style.height = '2em';
     firstButton.style.margin = '30px';
     firstButton.style.float = 'left';
+
 document.body.appendChild(firstButton);
 var squares = ('div');
 var numberOfSquares = document.getElementsByTagName(squares);
-var squaresArray = []; //stores all of my divs in an array
 
 firstButton.addEventListener('click', function() {
     var squares = document.createElement('div');
     squares.className = ('squares');
     squares.id = (numberOfSquares.length);
-    squaresArray.push(squares);
     document.body.appendChild(squares);
-    console.log(squaresArray);
     squares.addEventListener('mouseover', mouseOver);
     squares.addEventListener('mouseout', mouseOut);
     function mouseOver () {
@@ -30,34 +28,22 @@ firstButton.addEventListener('click', function() {
     function mouseOut () {
         squares.innerText = "";
     };
-
     squares.addEventListener('click', function() {
         // this.className = ('newColor');
         this.style.backgroundColor = randomColor();
     });
-
     squares.addEventListener('dblclick', function() {
-        var i = squares.id;
-        if (squares.id % 2 === 0) {
-            i++;
-            console.log(squaresArray);
-            //element after square removed
-            
-            console.log(true);
-
-        }
-
-        else  {
-            //element before square removed
-            console.log(false);
-        }
-    })
-
-
-
-
+        nextSquare = squares.nextSibling;
+        lastSquare = squares.previousSibling;
+        if (squares.id % 2 === 0 && nextSquare !== null) {
+            nextSquare.remove();
+        } else if (squares.id % 2 !== 0 && lastSquare !== firstButton) {
+                lastSquare.remove();
+            } else {
+                alert('No box available');
+            }
+    });
 });
-
 // random color generator
 function randomColor(){
     var allowed = "0369cf".split( '' ), s = "#";
